37ce181dae779713bd6e8380d741e9ac315e7305
Reyes1230 <lreyesp1230@gmail.com>
2026-09-05 08:52:02 -0500

    Preguntas

--- polls/views.py
+++ polls/views.py
@@ -4,22 +4,23 @@ from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 
-
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    return HttpResponse("Estas mirando la pregunta con id: %s." % question_id)
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
+    response = "Estas mirando los resultados de la pregunta con id: %s."
     return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return HttpResponse("Estas votando en la pregunta con id: %s." % question_id)
+
+def preguntas(request):
+    question = Question.objects.get(id=2)
+    return HttpResponse(question.question_text)
